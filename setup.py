2ef468a58208c72e9d79f5af269d9594dae2b082
Anil <nanil6304@gmail.com>
2025-07-02 22:16:18 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Anil kumar",
+    author_email="nanil6304@gmail.com",
+    packages=find_packages()
+)
